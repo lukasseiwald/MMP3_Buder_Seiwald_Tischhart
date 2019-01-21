d31d3be8409be302d46211f281ea34a327c206b8
Lukas Seiwald <lseiwald.mmt-b2016@fh-salzburg.ac.at>
2019-01-21 15:55:38 +0100

    feat: player animations

--- src/js/levels/Level1.js
+++ src/js/levels/Level1.js
@@ -6,17 +6,21 @@ import lang from '../lang'
 export default class extends Phaser.State {
   init() { }
   preload() {
-    this.load.spritesheet('dude', '../../assets/dude.png', { frameWidth: 32, frameHeight: 48 }); 
+    this.load.spritesheet('dude', '../../assets/dude.png', 48, 48, 48); 
   }
 
   create() {
     this.airconsole = new AirConsole()
 
-    this.player = this.game.add.sprite(100, 450, 'dude');
-    this.game.physics.enable([this.player], Phaser.Physics.ARCADE);
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.player.body.collideWorldBounds = true;
-    this.player.body.bounce.set(0.9);
+    //Players
+    this.players = this.game.add.group();
+    this.players.enableBody = true;
+
+    //Player 1
+    this.player1 = this.createPlayer(800, 550, 'dude', false, 650, 210);  //x, y, skin, facingRight?, jump (jumpHeight), velocity (runningSpeed)
+    this.player1.enableBody = true;
 
     // console.log(this.player)
 
@@ -42,17 +46,21 @@ export default class extends Phaser.State {
   }
 
   update() {
-    let mushroom = this.mushroom
-    let player = this.player
+    //let mushroom = this.mushroom
+    let player = this.player1
+
     this.airconsole.onMessage = function(device_id, data) {
-      console.log(player)
       // let player = airconsole.convertDeviceIdToPlayerNumber(device_id);
       if (data.move !== undefined && data.move === 'right') {
-        player.x += 4
+        //player.animations.play('left');
+        player.facingRight = false; 
+        player.body.velocity.x = -player.v;
         // player.animations.play('right', true);
       }
       else if (data.move !== undefined && data.move === 'left') {
-        player.x -= 4
+        //player.animations.play('right');
+        player.facingRight = true; 
+        player.body.velocity.x = player.v;
         // player.animations.play('left', true);
       }
       else
@@ -62,6 +70,37 @@ export default class extends Phaser.State {
       }
       console.log(player.body.velocity)
     };
+  }
+
+  createPlayer(x,y,skin,facingRight,jump,velocity) {
+
+    var player = this.players.create(x,y, skin);
+
+    //cover floor with tiles
+   
+    player.body.gravity.y = 500;
+    player.body.bounce.y = 0.1;
+    player.body.collideWorldBounds = true;
+    player.body.setSize(28, 45);
+
+    player.j = jump;
+    player.v = velocity;
+    player.facingRight = facingRight
+    player.attack = false;
+
+    player.animations.add('idleRight', [32,33,34,35], 12, true);
+    player.animations.add('idleLeft', [0,1,2,3], 12, true);
+    player.animations.add('left', [8,9,10,11,12,13,14,15], 12, true);
+    player.animations.add('right', [41,42,43,44,45,46,47,48], 12, true);
+    player.animations.add('attackRight', [16,17,18,19], 12, false);
+    player.animations.add('attackLeft', [20,21,22,23], 12, false);
+    player.animations.add('duck', [26,27,28,29,30], 12, true);
+    player.animations.add('jump', [37], 20, true);
+    player.animations.add('die', [38], 9, true);
+
+    player.animations.play('idleRight');
+
+    return player;
   }
 
   render() {

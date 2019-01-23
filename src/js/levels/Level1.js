4ef5de14f9c35be6737f82cda02060b1cfa63003
Lukas Seiwald <lseiwald.mmt-b2016@fh-salzburg.ac.at>
2019-01-23 13:08:21 +0100

    feat: game phsysics changed to p2 and colission detection added.

--- src/js/levels/Level1.js
+++ src/js/levels/Level1.js
@@ -5,32 +5,64 @@ import lang from '../lang'
 
 export default class extends Phaser.State {
   init() { }
+
   preload() {
+    this.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
+    this.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
+    this.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
+
     this.load.spritesheet('dude', '../../assets/dude.png', 48, 48, 48); 
     this.load.spritesheet('tileSet', 'assets/images/tileSet.png', 48, 48, 2);
-  }
+  };
 
   create() {
     this.airconsole = new AirConsole();
 
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
+    this.game.physics.p2.setImpactEvents(true);
+    this.game.physics.p2.gravity.y = 350;
+
+    this.game.physics.p2.updateBoundsCollisionGroup();
+
+    //Map
+    this.map = this.game.add.tilemap('map');
+    this.map.addTilesetImage('ground_1x1');
+    this.map.addTilesetImage('walls_1x2');
+    this.map.addTilesetImage('tiles2');
+
+    this.layer = this.map.createLayer('Tile Layer 1');
+    this.layer.resizeWorld();
+
+    //  Set the tiles for collision.
+    //  Do this BEFORE generating the p2 bodies below.
+    this.map.setCollisionBetween(1, 12);
+
+    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+    //  This call returns an array of body objects which you can perform addition actions on if
+    //  required. There is also a parameter to control optimising the map build.
+    this.game.physics.p2.convertTilemap(this.map, this.layer);
 
     //Players
     this.players = this.game.add.group();
     this.players.enableBody = true;
+    this.players.physicsBodyType = Phaser.Physics.P2JS;
 
     //Player 1
-    this.player1 = this.createPlayer(800, 550, 'dude', false, 650, 5);  //x, y, skin, facingRight?, jump (jumpHeight), velocity (runningSpeed)
-    this.player1.enableBody = true;
+    this.player1 = this.createPlayer(400, 350, 'dude', false, 50, 5);  //x, y, skin, facingRight?, jump (jumpHeight), velocity (runningSpeed)
+    this.game.physics.p2.enable(this.player1);
+    this.player1.body.fixedRotation = true;
+
+    this.game.physics.p2.setBoundsToWorld(true, true, true, true, false);
 
     //Platform
-    this.platforms = this.game.add.physicsGroup();
-    this.platforms.enableBody = true;
+    // this.platforms = this.game.add.physicsGroup();
+    // this.platforms.enableBody = true;
 
     //Tiles
-    this.tiles = this.game.add.physicsGroup();
-    this.tiles.enableBody = true;
-    this.createPlatforms();
+    // this.tiles = this.game.add.physicsGroup();
+    // this.tiles.enableBody = true;
+    // this.createPlatforms();
   }
 
   update() {
@@ -45,22 +77,28 @@ export default class extends Phaser.State {
       // }
       // let player = airconsole.convertDeviceIdToPlayerNumber(device_id);
       if (data.move !== undefined && data.move === 'right') {
-        player.animations.play('right');
-        player.facingRight = true;
-        player.x += player.v;
+        //player.body.moveRight(200);
+        player.x -= player.v ;
+        if(player.facingRight = false) {
+          player.animations.play('right');
+          player.facingRight = true;
+        }
       }
       else if (data.move !== undefined && data.move === 'left') {
-        player.animations.play('left');
-        player.facingRight = false;
-        player.x -= player.v ;
+        //player.body.moveLeft(200);
+        player.x += player.v ;
+        if(player.facingRight = true) {
+          player.animations.play('left');
+          player.facingRight = false;
+        }
       }
       else if (data.move !== undefined && data.move === 'up') {
         player.animations.play('jump');
-        player.y -= player.v ;
+        player.y -= player.j;
       }
       else
       {
-          player.body.velocity.setTo(0, player.body.velocity.y);
+          player.body.velocity.x = 0;
           if(player.facingRight) {
              player.animations.play('idleRight');
           }
@@ -73,7 +111,7 @@ export default class extends Phaser.State {
 
   playersUpdate() {
     //Coliding Player with Platform
-    this.game.physics.arcade.collide(this.players, this.platforms );
+    //this.game.physics.arcade.collide(this.players, this.platforms );
     //Colliding Players
     //this.game.physics.arcade.collide(this.players, this.players );
   }

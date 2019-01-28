eeb0c6062d0e7677a2a09c8168d631963cba2252
Denise Buder <d.buder@sbg.at>
2019-01-28 10:49:41 +0100

    feat: add controll stick to controller - not working perfetctly!
    #14

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,56 @@
 import {isTouchDevice} from './utils';
+let Draggable = require ('Draggable');
+
+let stick = document.getElementsByClassName('movement__stick')[0];
+
+document.getElementsByClassName('controller')[0].addEventListener('touchmove', function(event) {
+  for (var i = 0; i < event.targetTouches; i++) {
+    var touch = event.targetTouches[i];
+    console.log('touched ' + touch.identifier);
+  }
+}, false);
+
+let options = {
+  limit: function (
+  x,  // current X coordinate
+  y,  // current Y coordinate
+  x0, // original X coordinate (where drag was started)
+  y0  // original Y coordinate (where drag was started)
+) {
+
+  var radius = 75,
+    dx = x - x0,
+    dy = y - y0,
+    distance = Math.sqrt(dx*dx + dy*dy),
+
+    // only allow dragging within a circle of radius 100
+    outOfRange = distance > radius;
+
+
+  // if our point is outside of the circle, compute the
+  // point on the circle's edge closest to our point
+  if (outOfRange) {
+    x = x0 + radius * (x - x0) / distance;
+    y = y0 + radius * (y - y0) / distance;
+  }
+
+  return {
+    x: x,
+    y: y
+  };
+
+}
+}
+
+new Draggable(stick, options);
+
+
+
+class Stick {
+  constructor() {
+    console.log('Stick initialized');
+  }
+}
 
 let airConsole = new AirConsole({"orientation": "landscape"});
 
@@ -21,9 +73,13 @@ let buttons = document.getElementsByClassName('button');
 if (isTouchDevice()) {
   for (let button of buttons) {
     button.addEventListener("touchstart", function(e){
+      e.preventDeault();
+      e.stopPropagation();
       sendToScreen(e.currentTarget.dataset.direction);
     },{passive: true});
     button.addEventListener("touchend", function(e){
+      e.preventDeault();
+      e.stopPropagation();
       sendToScreen('idle');
     });
   }
@@ -38,3 +94,5 @@ else {
     });
   }
 }
+
+new Stick();

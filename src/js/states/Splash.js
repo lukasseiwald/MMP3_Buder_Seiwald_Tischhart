c686b549578c3332f7dabadd14721dccfa9eb8e6
Denise Buder <d.buder@sbg.at>
2019-04-12 13:20:56 +0200

    fix: changed dev variable to false

--- src/js/states/Splash.js
+++ src/js/states/Splash.js
@@ -17,7 +17,7 @@ export default class extends Phaser.State {
   }
 
   create () {
-    window.game.global.dev = true;
+    window.game.global.dev = false;
     window.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     window.game.global.healthBars = [];
 

08c63fd5bc4818c191ca37163c3dc0eb572af86b
Denise Buder <d.buder@sbg.at>
2019-05-21 14:46:05 +0200

    refactor: apply linter rules and change code style

--- src/js/sprites/Soul.js
+++ src/js/sprites/Soul.js
@@ -1,35 +1,37 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
 export default class Soul {
+	constructor (player, asset) {
+		this.scale = window.game.global.scale;
+		this.unit = window.game.global.unit;
+		this.soul = window.game.add.sprite(player.x, player.y - this.unit, asset);
+		this.soul.scale.setTo(this.scale, this.scale);
+		this.soul.enableBody = true;
+		window.game.physics.p2.enable(this.soul);
+		this.soul.physicsBodyType = Phaser.Physics.P2JS;
+		// to ensure that the soul only moves when it is obtained
+		this.soul.body.static = true;
+		// Hit Box
+		this.soul.body.setCircle(0.8 * this.unit);
 
-  constructor (player, asset) {
-    this.scale = window.game.global.scale;
-    this.unit = window.game.global.unit;
-    this.soul = game.add.sprite(player.x ,player.y - this.unit, asset);
-    this.soul.scale.setTo(this.scale, this.scale);
-    this.soul.enableBody = true;
-    window.game.physics.p2.enable(this.soul);
-    this.soul.physicsBodyType = Phaser.Physics.P2JS;
-    this.soul.body.static = true; //to ensure that the soul only moves when it is obtained
-    this.soul.body.setCircle(.8 * this.unit); //Hit Box
+		// Collisions for Souls
+		const soulCollisionGroup = window.game.physics.p2.createCollisionGroup();
+		const baseCollisionGroup = window.game.physics.p2.createCollisionGroup();
+		const playerCollisionGroup = window.game.physics.p2.createCollisionGroup();
 
-    //Collisions for Souls
-    let soulCollisionGroup = window.game.physics.p2.createCollisionGroup();
-    soulCollisionGroup.mask = 32;
-    let baseCollisionGroup = window.game.physics.p2.createCollisionGroup();
-    baseCollisionGroup.mask = 64;
-    let playerCollisionGroup = window.game.physics.p2.createCollisionGroup();
-    playerCollisionGroup.mask = 4;
+		soulCollisionGroup.mask = 32;
+		baseCollisionGroup.mask = 64;
+		playerCollisionGroup.mask = 4;
 
-    this.soul.body.setCollisionGroup(soulCollisionGroup);
-    this.soul.body.collides(playerCollisionGroup);
-    this.soul.body.collides(baseCollisionGroup, this.basedSoul, this);
-    this.soul.body.data.gravityScale = 0;
-    this.soul.fixedX = false;
-    this.soul.fixedY = false;
-    this.soul.beingCarried = false;
+		this.soul.body.setCollisionGroup(soulCollisionGroup);
+		this.soul.body.collides(playerCollisionGroup);
+		this.soul.body.collides(baseCollisionGroup, this.basedSoul, this);
+		this.soul.body.data.gravityScale = 0;
+		this.soul.fixedX = false;
+		this.soul.fixedY = false;
+		this.soul.beingCarried = false;
 
-    this.soul.animations.add('souling');
-    this.soul.animations.play('souling', 5, true);
-  }
+		this.soul.animations.add('souling');
+		this.soul.animations.play('souling', 5, true);
+	}
 }
--- src/js/states/Boot.js
+++ src/js/states/Boot.js
@@ -3,98 +3,93 @@ import WebFont from 'webfontloader';
 import config from '../config';
 
 export default class extends Phaser.State {
-  init() {
-    this.stage.backgroundColor = '#EDEEC9';
-    this.fontsReady = false;
-    this.fontsLoaded = this.fontsLoaded.bind(this);
+	init() {
+		this.stage.backgroundColor = '#EDEEC9';
+		this.fontsReady = false;
+		this.fontsLoaded = this.fontsLoaded.bind(this);
 
-    //stop game from pausing when game window is not in focus
-    this.stage.disableVisibilityChange = true;
-  }
+		// stop game from pausing when game window is not in focus
+		this.stage.disableVisibilityChange = true;
+	}
 
-  preload() {
-    //this.game.world.setBounds(0, 0, this.game.world.width, this.game.world.bounds.bottom+150);
+	preload() {
+		// this.game.world.setBounds(0, 0, this.game.world.width, this.game.world.bounds.bottom+150);
 
-    //LOAD BACKGROUND
-    this.load.image('background1', '../../assets/images/background/BG-1.png');
-    this.load.image('background2', '../../assets/images/background/BG-2.png');
-    this.load.image('background3', '../../assets/images/background/BG-3.png');
-    this.load.image('smoke', '../../assets/images/background/smoke.png');
+		// LOAD BACKGROUND
+		this.load.image('background1', '../../assets/images/background/BG-1.png');
+		this.load.image('background2', '../../assets/images/background/BG-2.png');
+		this.load.image('background3', '../../assets/images/background/BG-3.png');
+		this.load.image('smoke', '../../assets/images/background/smoke.png');
 
-    //LOAD CHARACTER SELECTION IMAGES
-    this.load.image('backgroundCharacterSelection', '../../assets/images/characterSelection/characterSelectionFrame.png');
-    this.load.image('characterPlateau', '../../assets/images/characterSelection/characterPlateau.png');
-    this.load.image('characterSilhouette', '../../assets/images/characterSelection/characterSilhouette.png');
-    // this.load.spritesheet('characterSilhouette', '../../assets/images/characterSelection/characterSilhouette2.png', 43, 75, 13);
+		// LOAD CHARACTER SELECTION IMAGES
+		this.load.image('backgroundCharacterSelection', '../../assets/images/characterSelection/characterSelectionFrame.png');
+		this.load.image('characterPlateau', '../../assets/images/characterSelection/characterPlateau.png');
+		this.load.image('characterSilhouette', '../../assets/images/characterSelection/characterSilhouette.png');
+		// this.load.spritesheet('characterSilhouette', '../../assets/images/characterSelection/characterSilhouette2.png', 43, 75, 13);
 
-    //LOAD TILES
-    this.load.spritesheet('tiles', '../../assets/tileMap/tileSet2.png', 33, 33, 12, 0, 4);
+		// LOAD TILES
+		this.load.spritesheet('tiles', '../../assets/tileMap/tileSet2.png', 33, 33, 12, 0, 4);
 
-    //LOAD BASES
-    this.load.image('egyptian_base', '../../assets/bases/egyptian_base.png');
-    this.load.image('knight_base', '../../assets/bases/knight_base.png');
-    this.load.image('lucifer_base', '../../assets/bases/lucifer_base.png');
-    this.load.image('kickapoo_base', '../../assets/bases/kickapoo_base.png');
+		// LOAD BASES
+		this.load.image('egyptian_base', '../../assets/bases/egyptian_base.png');
+		this.load.image('knight_base', '../../assets/bases/knight_base.png');
+		this.load.image('lucifer_base', '../../assets/bases/lucifer_base.png');
+		this.load.image('kickapoo_base', '../../assets/bases/kickapoo_base.png');
 
-    //LOAD PLAYERS
-    this.load.atlasJSONHash('egyptian', '../../assets/characters/egyptian/egyptian.png', '../../assets/characters/egyptian/egyptian.json');
-    this.load.atlasJSONHash('knight', '../../assets/characters/knight/knight.png', '../../assets/characters/knight/knight.json');
-    this.load.atlasJSONHash('lucifer', '../../assets/characters/lucifer/lucifer.png', '../../assets/characters/lucifer/lucifer.json');
-    this.load.atlasJSONHash('kickapoo', '../../assets/characters/kickapoo/kickapoo.png', '../../assets/characters/kickapoo/kickapoo.json');
+		// LOAD PLAYERS
+		this.load.atlasJSONHash('egyptian', '../../assets/characters/egyptian/egyptian.png', '../../assets/characters/egyptian/egyptian.json');
+		this.load.atlasJSONHash('knight', '../../assets/characters/knight/knight.png', '../../assets/characters/knight/knight.json');
+		this.load.atlasJSONHash('lucifer', '../../assets/characters/lucifer/lucifer.png', '../../assets/characters/lucifer/lucifer.json');
+		this.load.atlasJSONHash('kickapoo', '../../assets/characters/kickapoo/kickapoo.png', '../../assets/characters/kickapoo/kickapoo.json');
 
-    //LOAD SOULS
-    this.load.spritesheet('egyptian_soul', '../../assets/characters/egyptian/egyptian_soul.png', 50, 50, 2);
-    this.load.spritesheet('knight_soul', '../../assets/characters/knight/knight_soul.png', 50, 50, 2);
-    this.load.spritesheet('lucifer_soul', '../../assets/characters/lucifer/lucifer_soul.png', 50, 50, 2);
-    this.load.spritesheet('kickapoo_soul', '../../assets/characters/kickapoo/kickapoo_soul.png', 50, 50, 2);
+		// LOAD SOULS
+		this.load.spritesheet('egyptian_soul', '../../assets/characters/egyptian/egyptian_soul.png', 50, 50, 2);
+		this.load.spritesheet('knight_soul', '../../assets/characters/knight/knight_soul.png', 50, 50, 2);
+		this.load.spritesheet('lucifer_soul', '../../assets/characters/lucifer/lucifer_soul.png', 50, 50, 2);
+		this.load.spritesheet('kickapoo_soul', '../../assets/characters/kickapoo/kickapoo_soul.png', 50, 50, 2);
 
-    //LOAD BULLETS
-    this.load.image('egyptian_bullet', '../../assets/characters/egyptian/egyptian_bullet.png');
-    this.load.image('knight_bullet', '../../assets/characters/knight/knight_bullet.png');
-    this.load.image('lucifer_bullet', '../../assets/characters/lucifer/lucifer_bullet.png');
-    this.load.image('kickapoo_bullet', '../../assets/characters/kickapoo/kickapoo_bullet.png');
+		// LOAD BULLETS
+		this.load.image('egyptian_bullet', '../../assets/characters/egyptian/egyptian_bullet.png');
+		this.load.image('knight_bullet', '../../assets/characters/knight/knight_bullet.png');
+		this.load.image('lucifer_bullet', '../../assets/characters/lucifer/lucifer_bullet.png');
+		this.load.image('kickapoo_bullet', '../../assets/characters/kickapoo/kickapoo_bullet.png');
 
-    //LOAD ITEMS
-    this.load.spritesheet('health_item', '../../assets/items/health.png', 50, 50, 5);
-    this.load.spritesheet('jump_item', '../../assets/items/jump.png', 50, 50, 7);
-    this.load.spritesheet('speed_item', '../../assets/items/speed.png', 50, 50, 4);
-    this.load.spritesheet('shield_item', '../../assets/items/shield.png', 50, 50, 5);
-    this.load.image('shield_character', '../../assets/items/shield2.png');
+		// LOAD ITEMS
+		this.load.spritesheet('health_item', '../../assets/items/health.png', 50, 50, 5);
+		this.load.spritesheet('jump_item', '../../assets/items/jump.png', 50, 50, 7);
+		this.load.spritesheet('speed_item', '../../assets/items/speed.png', 50, 50, 4);
+		this.load.spritesheet('shield_item', '../../assets/items/shield.png', 50, 50, 5);
+		this.load.image('shield_character', '../../assets/items/shield2.png');
 
-    //LOAD CHARACTER HEAD FOR SCORE
-    this.load.image('egyptian_head', '../../assets/images/controller/egyptian.png');
-    this.load.image('knight_head', '../../assets/images/controller/knight.png');
-    this.load.image('lucifer_head', '../../assets/images/controller/lucifer.png');
-    this.load.image('kickapoo_head', '../../assets/images/controller/kickapoo.png');
+		// LOAD CHARACTER HEAD FOR SCORE
+		this.load.image('egyptian_head', '../../assets/images/controller/egyptian.png');
+		this.load.image('knight_head', '../../assets/images/controller/knight.png');
+		this.load.image('lucifer_head', '../../assets/images/controller/lucifer.png');
+		this.load.image('kickapoo_head', '../../assets/images/controller/kickapoo.png');
 
-    //LOADING EXTRAS
-    this.load.spritesheet('dash_smoke', '../../assets/extras/dash/dash_smoke.png', 60, 60, 9);
+		// LOADING EXTRAS
+		this.load.spritesheet('dash_smoke', '../../assets/extras/dash/dash_smoke.png', 60, 60, 9);
 
-    //LOADING FONTS
+		if(config.webfonts.length) {
+			WebFont.load({
+				google: {
+					families: config.webfonts
+				},
+				active: this.fontsLoaded
+			});
+		}
+	}
 
-    //LOAD MUSIC
-    //this.load.audio('start_background_music', 'assets/audio/background_music/the_dark_amulet.mp3');
+	render() {
+		if (config.webfonts.length && this.fontsReady) {
+			this.state.start('Splash');
+		}
+		if (!config.webfonts.length) {
+			this.state.start('Splash');
+		}
+	}
 
-    if (config.webfonts.length) {
-      WebFont.load({
-        google: {
-          families: config.webfonts
-        },
-        active: this.fontsLoaded
-      });
-    }
-  }
-
-  render() {
-    if (config.webfonts.length && this.fontsReady) {
-      this.state.start('Splash');
-    }
-    if (!config.webfonts.length) {
-      this.state.start('Splash');
-    }
-  }
-
-  fontsLoaded() {
-    this.fontsReady = true;
-  }
+	fontsLoaded() {
+		this.fontsReady = true;
+	}
 }

fc3c40e9dc50d1e0715bf3ddb55d122d40d66830
Denise Buder <d.buder@sbg.at>
2019-05-22 11:50:06 +0200

    refactor(ControllerStateManager): coding conventions and code style

--- src/js/controllerStateManager.js
+++ src/js/controllerStateManager.js
@@ -1,23 +1,24 @@
-export default class controllerStateManager {
-  constructor(containerId) {
-    this.states = new Map();
-    this.currentState = undefined;
-    this.container = document.getElementById(containerId);
-  }
+export default class ControllerStateManager {
+	constructor(containerId) {
+		this.states = new Map();
+		this.currentState = undefined;
+		this.container = document.getElementById(containerId);
+	}
 
-  setState(name, stateId) {
-    let node = document.getElementById(stateId);
-    this.states.set(name, node);
-    node.parentNode.removeChild(node);
-  }
+	setState(name, stateId) {
+		const node = document.getElementById(stateId);
 
-  startState(name) {
-    if (this.currentState) {
-      let prev = this.states.get(this.currentState);
-      prev.parentNode.removeChild(prev);
-    }
+		this.states.set(name, node);
+		node.parentNode.removeChild(node);
+	}
 
-    this.currentState = name;
-    this.container.appendChild(this.states.get(name));
-  }
+	startState(name) {
+		if (this.currentState) {
+			const prev = this.states.get(this.currentState);
+
+			prev.parentNode.removeChild(prev);
+		}
+		this.currentState = name;
+		this.container.appendChild(this.states.get(name));
+	}
 }

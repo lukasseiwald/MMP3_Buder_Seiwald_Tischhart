8095b999fc50c49b03def1baa5f6d793ed763a13
Denise Buder <d.buder@sbg.at>
2019-05-24 10:53:10 +0200

    refactor: coding conventions & code style

--- src/js/controller.js
+++ src/js/controller.js
@@ -74,7 +74,6 @@ function handleGame(data) {
 		setUpEmotes();
 		break;
 	case 'reconnected':
-		console.log('heeey')
 		csm.startState('game');
 		setUpController();
 		break;
@@ -223,9 +222,10 @@ function setUpController() {
 
 	function launchShoot() {
 		endTime = new Date();
-	  let shootTime = endTime - startTime;
-	  sendToScreen({action: buttonShoot.dataset.direction, shootTime: shootTime});
-	  buttonShoot.classList.remove('button--active');
+		const shootTime = endTime - startTime;
+
+		sendToScreen({action: buttonShoot.dataset.direction, shootTime: shootTime});
+		buttonShoot.classList.remove('button--active');
 	}
 
 	buttonShoot.addEventListener('touchstart', prepareShoot);
